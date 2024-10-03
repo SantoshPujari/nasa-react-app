8c16054382991769960f5af08f8ca51ac714b5c1
Santosh Pujari <santosh.pujari@hotmail.com>
2024-10-03 12:10:45 -0500

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h2>The Brutal Martian Landscape</h2>
+        <h1>APOD PROJECT</h1>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <div>
+      <img src='mars.png' alt='mar-demo-picture' />
+    </div>
+  );
+};
+
+export default Main;
